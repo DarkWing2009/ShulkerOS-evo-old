347cfe9ca2fdfb7cd31b47d7574684910b0bdfc5
AJohnsontcs <89605606+DarkWing2009@users.noreply.github.com>
2021-11-11 22:52:42 +0000

    Added game 2 and created UI.

--- shulkeros++.sh
+++ shulkeros++.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
-
-python ./guessTheNumber.py
+echo "print(What Game Would You Like To Play?:
+1. Guess The Number
+2. New RPG
+3. Rock Paper Scissors
+4. Business Simulator
+5. Kingdom Sim"
+read Choice
+clear -x
+case $Choice in
+  1)
+    python ./guessTheNumber.py
+    ;;
+  2)
+    python ./NewRPG.py
+    ;;
+  3 | 4| 5)
+    echo "Sorry, coming soon"
+    ;;
+  *)
+    echo "Don't do that, you are wasting your computer memory"
+esac
